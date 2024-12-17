06024faa111af2f5cdd640a424f5b836b8035f18
Tom MacWright <tom@macwright.com>
2024-12-17 13:26:56 -0500

    Update to helper

--- src/index.test.ts
+++ src/index.test.ts
@@ -12,33 +12,46 @@ async function reader(path: string) {
 		fetch: (...args: any[]) => {
 			return Promise.resolve(new Response(new Blob([m.get(args[0])!])));
 		},
-		async saveAs(_path: string) {},
+		async save() {},
 	};
 }
 
-function recorder() {
+function recorder(path: string) {
 	const recordings: Promise<[string, string]>[] = [];
 	return {
-		fetch: (...args: any[]) => {
-			console.log("fetching with args", args);
+		fetch: (...args: Parameters<typeof fetch>) => {
 			const url = args[0];
 			return fetch(...args).then((r) => {
-				recordings.push(
-					r
-						.clone()
-						.text()
-						.then((text) => {
-							return [url, text];
-						}),
-				);
+				if (typeof url === "string") {
+					recordings.push(
+						r
+							.clone()
+							.text()
+							.then((text) => {
+								return [url, text];
+							}),
+					);
+				} else {
+					console.error("Non-string URL provided unexpectedly");
+				}
 				return r;
 			});
 		},
 		recordings,
-		async saveAs(path: string) {
+		async save() {
 			await Fs.writeFile(path, JSON.stringify(await Promise.all(recordings)));
 		},
 	};
+}
+
+/**
+ * Using msw is probably the right thing to do, but there isn't an easy
+ * mode for recording & replaying Node-native fetch calls. This is that.
+ * It will probably need to be extended with the ability to communicate
+ * headers and such in the future.
+ */
+async function fixture(path: string, mode: "read" | "write") {
+	return await (mode === "read" ? reader(path) : recorder(path));
 }
 
 describe("ata", () => {
@@ -50,7 +63,7 @@ describe("ata", () => {
 			finish = resolve;
 		});
 
-		const rec = await reader("./tests/fixtures/lodash.json"); // recorder();
+		const rec = await fixture("./tests/fixtures/lodash.json", "read");
 
 		const ata = setupTypeAcquisition({
 			projectName: "My ATA Project",
@@ -78,6 +91,6 @@ describe("ata", () => {
 		ata('import { x } from "lodash"');
 
 		await expect(finishedPromise).resolves.toBeFalsy();
-		await rec.saveAs("./tests/fixtures/lodash.json");
+		await rec.save();
 	});
 });
