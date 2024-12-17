7d3b551613d594818c38432979b19b83598c540f
Tom MacWright <tom@macwright.com>
2024-12-17 12:17:38 -0500

    Remove jest config, enforce biome

--- demo/worker.ts
+++ demo/worker.ts
@@ -1,58 +1,58 @@
 import {
-  createDefaultMapFromCDN,
-  createSystem,
-  createVirtualTypeScriptEnvironment,
+	createDefaultMapFromCDN,
+	createSystem,
+	createVirtualTypeScriptEnvironment,
 } from "@typescript/vfs";
-import * as ts from "typescript";
-import * as Comlink from "comlink";
 import { createWorker } from "@valtown/codemirror-ts/worker";
+import * as Comlink from "comlink";
+import * as ts from "typescript";
 import { setupTypeAcquisition } from "../src/index";
 
 console.log("here");
 const worker = createWorker(
-  (async function () {
-    const fsMap = await createDefaultMapFromCDN(
-      { target: ts.ScriptTarget.ES2022 },
-      ts.version,
-      false,
-      ts,
-    );
-    const system = createSystem(fsMap);
+	(async () => {
+		const fsMap = await createDefaultMapFromCDN(
+			{ target: ts.ScriptTarget.ES2022 },
+			ts.version,
+			false,
+			ts,
+		);
+		const system = createSystem(fsMap);
 
-    const env = createVirtualTypeScriptEnvironment(system, [], ts, {
-      lib: ["ES2022"],
-    });
+		const env = createVirtualTypeScriptEnvironment(system, [], ts, {
+			lib: ["ES2022"],
+		});
 
-    const ata = setupTypeAcquisition({
-      projectName: "My ATA Project",
-      typescript: ts,
-      logger: console,
-      delegate: {
-        receivedFile: (code: string, path: string) => {
-          env.createFile(path, code);
-          // Add code to your runtime at the path...
-        },
-        started: () => {
-          console.log("ATA start");
-        },
-        progress: (downloaded: number, total: number) => {
-          console.log(`Got ${downloaded} out of ${total}`);
-        },
-        finished: (vfs) => {
-          console.log("ATA done", vfs);
-        },
-      },
-    });
+		const ata = setupTypeAcquisition({
+			projectName: "My ATA Project",
+			typescript: ts,
+			logger: console,
+			delegate: {
+				receivedFile: (code: string, path: string) => {
+					env.createFile(path, code);
+					// Add code to your runtime at the path...
+				},
+				started: () => {
+					console.log("ATA start");
+				},
+				progress: (downloaded: number, total: number) => {
+					console.log(`Got ${downloaded} out of ${total}`);
+				},
+				finished: (vfs) => {
+					console.log("ATA done", vfs);
+				},
+			},
+		});
 
-    console.log("here");
+		console.log("here");
 
-    return {
-      env,
-      onFileUpdated(env, path, code) {
-        ata(code);
-      },
-    };
-  })(),
+		return {
+			env,
+			onFileUpdated(env, path, code) {
+				ata(code);
+			},
+		};
+	})(),
 );
 
 Comlink.expose(worker);
--- tests/edgeCases.spec.ts
+++ tests/edgeCases.spec.ts
@@ -1,24 +1,24 @@
+import { describe, expect, it } from "vitest";
 import { mapModuleNameToModule } from "../src/edgeCases";
-import { describe, it, expect } from "vitest";
 
 describe(mapModuleNameToModule, () => {
-  it("gives node for known identifiers", () => {
-    expect(mapModuleNameToModule("fs")).toEqual("node");
-  });
+	it("gives node for known identifiers", () => {
+		expect(mapModuleNameToModule("fs")).toEqual("node");
+	});
 
-  it("handles the weird node: prefix", () => {
-    expect(mapModuleNameToModule("node:fs")).toEqual("node");
-  });
+	it("handles the weird node: prefix", () => {
+		expect(mapModuleNameToModule("node:fs")).toEqual("node");
+	});
 
-  it("handles mandatorily-prefixed node: identifiers", () => {
-    expect(mapModuleNameToModule("node:test")).toEqual("node");
-    expect(mapModuleNameToModule("test")).toEqual("test");
-  });
+	it("handles mandatorily-prefixed node: identifiers", () => {
+		expect(mapModuleNameToModule("node:test")).toEqual("node");
+		expect(mapModuleNameToModule("test")).toEqual("test");
+	});
 
-  it("strips module filepaths", () => {
-    expect(mapModuleNameToModule("lodash/identity")).toEqual("lodash");
-    expect(mapModuleNameToModule("@org/lodash/identity")).toEqual(
-      "@org/lodash",
-    );
-  });
+	it("strips module filepaths", () => {
+		expect(mapModuleNameToModule("lodash/identity")).toEqual("lodash");
+		expect(mapModuleNameToModule("@org/lodash/identity")).toEqual(
+			"@org/lodash",
+		);
+	});
 });
